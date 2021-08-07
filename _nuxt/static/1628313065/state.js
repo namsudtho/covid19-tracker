cccb1fa9d8b829544ef09cdf89fb767399424b9b
Anun Namsudtho <namsudtho@outlook.com>
2021-08-07 12:11:20 +0700

    c747629

---
+++ _nuxt/static/1628313065/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcovid19-tracker\u002F_nuxt\u002Fstatic\u002F1628313065",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcovid19-tracker\u002F",assetsPath:"\u002Fcovid19-tracker\u002F_nuxt\u002F",cdnURL:a}}}}(null));
